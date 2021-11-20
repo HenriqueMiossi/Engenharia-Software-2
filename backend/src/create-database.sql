a4778907514bffa9fb5b06c7214b0f1527856d34
HenriqueMiossi <henriquemiossi@gmail.com>
2021-11-20 06:46:20 -0800

    Feat: atualizado arquivo SQL com os requisitos do sprint 2

--- backend/src/create-database.sql
+++ backend/src/create-database.sql
@@ -1,16 +1,37 @@
-CREATE TABLE IF NOT EXISTS cliente (
-	id serial PRIMARY KEY,
-  	nome VARCHAR ( 50 ) NOT NULL,
-  	telefone VARCHAR ( 15 ) NOT NULL,
-  	cpf VARCHAR ( 14 ) UNIQUE NOT NULL
-);
-
+/* Referenciada por cliente e funcionario */
 CREATE TABLE IF NOT EXISTS endereco (
 	id serial PRIMARY KEY,
   	rua VARCHAR ( 50 ) NOT NULL,
   	cidade VARCHAR ( 50 ) NOT NULL,
   	numero INTEGER NOT NULL,
   	cep VARCHAR ( 10 ) NOT NULL,
-  	uf VARCHAR ( 2 ) NOT NULL,
-  	cliente_id INTEGER REFERENCES cliente ( id )
+  	uf VARCHAR ( 2 ) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cliente (
+	id serial PRIMARY KEY,
+  	nome VARCHAR ( 50 ) NOT NULL,
+  	telefone VARCHAR ( 15 ) NOT NULL,
+  	cpf VARCHAR ( 14 ) UNIQUE NOT NULL,
+	endereco_id INTEGER REFERENCES endereco ( id )
+);
+
+CREATE TABLE IF NOT EXISTS funcionario (
+	id serial PRIMARY KEY,
+  	matricula VARCHAR ( 10 ) UNIQUE NOT NULL,
+  	nome VARCHAR ( 50 ) NOT NULL,
+  	telefone VARCHAR ( 15 ) NOT NULL,
+  	cpf VARCHAR ( 14 ) UNIQUE NOT NULL,
+  	salarioBase REAL NOT NULL,
+	endereco_id INTEGER REFERENCES endereco ( id )
+);
+
+CREATE TABLE IF NOT EXISTS produto (
+	id serial PRIMARY KEY,
+  	codigo VARCHAR ( 10 ) NOT NULL,
+  	descricao VARCHAR ( 255 ) NOT NULL,
+  	valor REAL NOT NULL,
+    quantidadeEstoque INTEGER NOT NULL,
+    estoqueMinimo INTEGER NOT NULL,
+    validade DATE NOT NULL
 );
